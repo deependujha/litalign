a24448e6a5bbadf7d759e05860ca5715aee6d9cc
Deependu <deependujha21@gmail.com>
2025-06-16 09:43:17 +0000

    update

--- src/litalign/trainer.py
+++ src/litalign/trainer.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
+from typing import Any, List
 
 
 class BaseTrainer(ABC):
-    def __init__(self, *args, **kwargs):
+    def __init__(self) -> None:
         """Base class for trainers in the LitAlign framework.
 
         This class defines the interface for all trainers, ensuring that
@@ -11,13 +12,13 @@ class BaseTrainer(ABC):
         """
 
     @abstractmethod
-    def rollout(self, batch) -> None:
+    def rollout(self, batch: List[Any]) -> None:
         pass
 
     @abstractmethod
-    def evaluate(self, batch, responses) -> None:
+    def evaluate(self, batch: List[Any], responses: Any) -> None:
         pass
 
     @abstractmethod
-    def optimize(self, batch, responses, rewards) -> None:
+    def optimize(self, batch: List[Any], responses: Any, rewards: Any) -> None:
         pass
